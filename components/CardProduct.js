4e981707d96a8e6eb2cec4812ae7417d89b29d9a
Arthur <arthur.nascimento2020@outlook.com>
2022-07-09 18:04:59 -0300

    Criando um componente para o card do produto e alterando aparencia

---
+++ components/CardProduct.js
@@ -0,0 +1,58 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import styles from '../styles/CardProduct.module.css'
+
+export default function CardProduct() {
+    return (
+        <div className={`col-7 col-sm-5 col-sm-4 col-md-3 col-xxl-2 mx-4 mb-3 ${styles.cartao}`}>
+            <div id={styles.cartaoTopo} className="row justify-content-center py-2">
+                <p className='m-0'>FRETE GRÁTIS SUL E SUDESTE</p>
+            </div>
+            <Link href="/detalhe">
+                <a className="row justify-content-center py-2">
+                    <Image id="imagem-produto" src="/images/filtro-oleo-1.png" width="260px" height="230px" alt="" />
+                </a>
+            </Link>
+            <div className="row">
+                <h5 className={`m-0`}>Filtro Oleo Do Motor 1.0 Corsa Classic</h5>
+            </div>
+            <div className="row d-flex">
+                <div className="col-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#F19800" className="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                    </svg>
+                </div>
+                <div className="col-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#F19800" className="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                    </svg>
+                </div>
+                <div className="col-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#F19800" className="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                    </svg>
+                </div>
+                <div className="col-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#F19800" className="bi bi-star-half" viewBox="0 0 16 16">
+                        <path d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
+                    </svg>
+                </div>
+                <div className="col-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#F19800" className="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                    </svg>
+                </div>
+            </div>
+            <div className="row mt-3">
+                <p className='m-0'><span className={styles.span}>R$ 320,68</span></p>
+            </div>
+            <div className="row mt-1 mb-0 h4">
+                <h5 className="m-0">R$ 271,29</h5>
+            </div>
+            <div className="row mt-0 mb-2 p-0">
+                <p className="m-0">em Pix ou 1x no cartão</p>
+            </div>
+        </div>
+    )
+}
--- components/Header.js
+++ components/Header.js
@@ -7,11 +7,12 @@ import styles from '../styles/Header.module.css'
 export default function Header() {
     return (
 
-        <header>
+        <header className={styles.header}>
             <div className="row align-items-center py-2">
                 <Link href="/">
-                    <a className="col-2 col-md-1 offset-md-1 offset-lg-2">
-                        <Image className="img-fluid" id="logo" src="/images/logoVenenoBW.png" width="130px" height="70px" alt="logo" />
+                    <a className="col-2 col-md-1 offset-md-1 offset-lg-2 p-2 text-center">
+                        <h3 className='m-0'><b className={styles.titulo}>CLASSIC</b></h3>
+                        <p className={`m-0 ${styles.subtitulo}`}>Auto Peças</p>
                     </a>
                 </Link>
                 <div className="col-6 text-center">
@@ -33,7 +34,7 @@ export default function Header() {
                         <div className="col">
                             <Link href="/cesta">
                                 <a>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#e23918" className="bi bi-basket2-fill" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#4E5166" className="bi bi-basket2-fill" viewBox="0 0 16 16">
                                         <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383L5.93 1.757zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0v-2zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0v-2zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1z" />
                                     </svg>
                                 </a>
@@ -42,7 +43,7 @@ export default function Header() {
                         <div className="col">
                             <Link href="/login">
                                 <a >
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#e23918" className="bi bi-person" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#4E5166" className="bi bi-person" viewBox="0 0 16 16">
                                         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                                     </svg>
                                 </a>
